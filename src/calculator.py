90ce9b0438d213f0d0323da2625cc23266f3518e
ja3697 <ja3697@nau.edu>
2024-06-21 15:23:35 -0700

    Update calculator.py

--- src/calculator.py
+++ src/calculator.py
@@ -3,8 +3,7 @@ def add(a, b):
 def subtract(a, b):
   return a - b
 def divide (a,b):
-  if(a == 1 && b == 0):
-    print(f'The division {a} / {b} is not possible')
-    return
+  if(b == 0):
+    print('The division is not possible')
   else:
     return a / b
